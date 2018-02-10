c4eb6eaee9f373b581db750cfe5a00a03396a567
nightfall1024 <35345905+nightfall1024@users.noreply.github.com>
2018-02-10 10:58:57 -0600

    Create myTest1.js

---
+++ myTest1.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var a127 = require('a127-magic');
+var express = require('express');
+var app = express();
+
+a127.init(function(config) {
+  app.use(a127.middleware(config));
+
+  app.listen(process.env.PORT || 10010);
+
+  console.log('try this:\nhttp://127.0.0.1:10010/weather?city=Kinston,NC');
+});
